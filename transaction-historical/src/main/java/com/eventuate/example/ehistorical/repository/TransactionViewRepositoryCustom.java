87a2b6469e62479f98e14f693a98530507019eb1
camapac <doduyloi@gmail.com>
2020-01-13 09:29:24 +0700

    add docker file

--- transaction-historical/src/main/java/com/eventuate/example/ehistorical/repository/TransactionViewRepositoryCustom.java
+++ transaction-historical/src/main/java/com/eventuate/example/ehistorical/repository/TransactionViewRepositoryCustom.java
@@ -1,5 +1,8 @@
 package com.eventuate.example.ehistorical.repository;
 
+import com.eventuate.example.constant.TransactionState;
+
 public interface TransactionViewRepositoryCustom {
 
+	public void updateTheState(String id, TransactionState state);
 }
--- transaction-historical/src/main/java/com/eventuate/example/ehistorical/repository/impl/TransactionViewRepositoryImpl.java
+++ transaction-historical/src/main/java/com/eventuate/example/ehistorical/repository/impl/TransactionViewRepositoryImpl.java
@@ -2,8 +2,15 @@ package com.eventuate.example.ehistorical.repository.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
+import com.eventuate.example.constant.TransactionState;
+import com.eventuate.example.ehistorical.document.TransactionView;
 import com.eventuate.example.ehistorical.repository.TransactionViewRepositoryCustom;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
 
 public class TransactionViewRepositoryImpl implements TransactionViewRepositoryCustom {
 
@@ -13,4 +20,14 @@ public class TransactionViewRepositoryImpl implements TransactionViewRepositoryC
 	public TransactionViewRepositoryImpl(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
 	}
+
+	@Override
+	public void updateTheState(String id, TransactionState state) {
+		 mongoTemplate.upsert(new Query(where("id").is(id)),
+		            new Update().set("state", state)
+		            , TransactionView.class);
+
+	}
+	
+	
 }
